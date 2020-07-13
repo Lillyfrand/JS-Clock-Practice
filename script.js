694de058ad3c7e2d37f03edab48c314c8b534fad
Robert Wahlmeier <TheSilentFyrd@gmail.com>
2020-07-13 16:03:29 -0600

    added AM & PM

--- script.js
+++ script.js
@@ -2,33 +2,42 @@ var d,h,m,s,animate,ap;
 var sec = document.getElementById("sec")
 var min = document.getElementById("min")
 var hr = document.getElementById("hr")
-
+var amPm= document.getElementById("ap")
 function init(){
 	d=new Date();
 	h=d.getHours();
 	m=d.getMinutes();
 	s=d.getSeconds();
-	ap = "AM"
+	ap=" AM"
 	clock();
 };
 
 function clock(){
-	s++;
 	if(s==60){
-	s=0;
-	m++;
-		if(m==60){
+		s=0;
+		m++;
+	}
+	if(m==60){
 		m=0;
 		h++;
-			if(hr==24){
-			hr=0;
-			}
-
+	}
+	if(h>=24){
+		h=0
+	}
+	let temp=h
+	if(temp>=12){
+		if(temp!=12){ 
+			temp-=12;
 		}
+		ap="PM";
+	}else{ 
+		ap="AM";
 	}
 	sec.innerHTML=s.toString().padStart(2,0);
 	min.innerHTML=m.toString().padStart(2,0);
 	hr.innerHTML=h.toString().padStart(2,0);
+	amPm.innerHTML=ap.toString()
+	s++
 	animate=setTimeout(clock,1000);
 };
 window.onload=init;
